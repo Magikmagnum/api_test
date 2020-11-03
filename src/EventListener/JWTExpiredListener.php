226a787d4ce9d2712e22915699cbdbb9f58528b4
Ratisseur <ericgansa@outlook.com>
2020-11-03 11:00:36 +0100

    Update: Personnalisation  de la reponse sur le jeton de securiter expirer

---
+++ src/EventListener/JWTExpiredListener.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTExpiredEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Response\JWTAuthenticationFailureResponse;
+
+
+class JWTExpiredListener extends AbstractController
+{
+    /**
+     * @param JWTExpiredEvent $event
+     */
+    public function onJWTExpired(JWTExpiredEvent $event)
+    {
+        $response = new JWTAuthenticationFailureResponse($this->statusCode(Response::HTTP_UNAUTHORIZED, [], "Votre token a expiré, veuillez le renouveler."));
+        $event->setResponse($response);
+    }
+}
